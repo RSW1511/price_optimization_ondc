458e39600a432c6653a77cad9b45f4bfc9d63161
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-02-10 01:38:10 +0530

    webapp: made changes in urls.py in store app

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -1,8 +1,12 @@
 # store/urls.py
 
 from django.urls import path
+from django.urls import include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('store/', include('store.urls')),
+    
+
 ]
