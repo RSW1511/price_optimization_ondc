14143e17cde7a6a08f74d1b032f21c5c2c77fbc0
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-02-10 01:12:19 +0530

    webapp: registered models in admin

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Product, Customer, Transaction, CompetitorPrice, MarketData
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_id', 'name', 'type', 'category', 'shelf_life', 'inventory', 'price', 'min_selling_price']
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['customer_id', 'name', 'avg_time_to_buy']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['product_id', 'customer_id', 'timestamp', 'action']
+
+@admin.register(CompetitorPrice)
+class CompetitorPriceAdmin(admin.ModelAdmin):
+    list_display = ['price_id', 'product_id', 'competitor_name', 'timestamp', 'price']
+
+@admin.register(MarketData)
+class MarketDataAdmin(admin.ModelAdmin):
+    list_display = ['product_id', 'total_sales', 'trends', 'avg_selling_price']
