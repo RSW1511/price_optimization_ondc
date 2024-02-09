ec2c2112df69d31ebdf6bcfebce43e975d0aac28
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-02-09 21:39:04 +0530

    webapp: changed models.py

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,40 +1,35 @@
 from django.db import models
 
-class Product:
-    def __init__(self, product_id, name, type_, category, shelf_life, inventory, price, min_selling_price):
-        self.product_id = product_id
-        self.name = name
-        self.type = type_
-        self.category = category
-        self.shelf_life = shelf_life
-        self.inventory = inventory
-        self.price = price
-        self.min_selling_price = min_selling_price
+class Product(models.Model):
+    product_id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    shelf_life = models.IntegerField()
+    inventory = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    min_selling_price = models.DecimalField(max_digits=10, decimal_places=2)
 
-class Customer:
-    def __init__(self, customer_id, name, avg_time_to_buy):
-        self.customer_id = customer_id
-        self.name = name
-        self.avg_time_to_buy = avg_time_to_buy
+class Customer(models.Model):
+    customer_id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=100)
+    avg_time_to_buy = models.FloatField()
 
-class Transaction:
-    def __init__(self, product_id, customer_id, timestamp, action):
-        self.product_id = product_id
-        self.customer_id = customer_id
-        self.timestamp = timestamp
-        self.action = action
+class Transaction(models.Model):
+    product_id = models.CharField(max_length=50)
+    customer_id = models.CharField(max_length=50)
+    timestamp = models.DateTimeField()
+    action = models.CharField(max_length=50)
 
-class CompetitorPrice:
-    def __init__(self, price_id, product_id, competitor_name, timestamp, price):
-        self.price_id = price_id
-        self.product_id = product_id
-        self.competitor_name = competitor_name
-        self.timestamp = timestamp
-        self.price = price
+class CompetitorPrice(models.Model):
+    price_id = models.CharField(max_length=50, primary_key=True)
+    product_id = models.CharField(max_length=50)
+    competitor_name = models.CharField(max_length=100)
+    timestamp = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-class MarketData:
-    def __init__(self, product_id, total_sales, trends, avg_selling_price):
-        self.product_id = product_id
-        self.total_sales = total_sales
-        self.trends = trends
-        self.avg_selling_price = avg_selling_price
+class MarketData(models.Model):
+    product_id = models.CharField(max_length=50, primary_key=True)
+    total_sales = models.IntegerField()
+    trends = models.CharField(max_length=100)
+    avg_selling_price = models.DecimalField(max_digits=10, decimal_places=2)
